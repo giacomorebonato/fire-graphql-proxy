6c76d3e5ceabcef5899c123967641e87aae06b96
Giacomo Rebonato <grebonato@zendesk.com>
2016-12-08 12:15:14 +0000

    comment for headers

--- src/schema/categories/categoriesResolver.ts
+++ src/schema/categories/categoriesResolver.ts
@@ -2,6 +2,8 @@ import got = require('got')
 import snapshotToArray from '../../lib/snapshotToArray'
 
 function categoriesResolver (root, args: any, req) {
+  // req.header headers of original request
+  // you can forward here the Authorization to the new request
   return got(process.env['FIREBASE_URL'] + `categories.json`, { json: true })
       .then(res => snapshotToArray(res.body))
 }
